2a4d2f0179dcffb5c10cc522463911ee63303a45
Myth <kuangchengping@outlook.com>
2016-08-30 22:29:00 +0800

    本来都设计妥当了，只差SQL的执行结果了，结果就卡在了数据库上，基本功到底还是不扎实

--- src/cn/struts/download/DownloadAction.java
+++ src/cn/struts/download/DownloadAction.java
@@ -1,47 +1,68 @@
 package cn.struts.download;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.sql.ResultSet;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.ServletActionContext;
 
+import cn.hibernate.utils.Table2Class;
+
 import com.myth.mysql.Mysql;
+import com.myth.mysql.Table2Excel;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.util.ValueStack;
-
+/**
+ * 带了setget方法的属性会在URL链接中自动获取到值，如果没有就是null
+ * @author  Myth
+ * @date 2016年8月30日 下午8:55:44
+ * @TODO 下载的action，依据参数来生成对应Excel文件再下载
+ */
 @SuppressWarnings("serial")
 public class DownloadAction extends ActionSupport {
-	String filetype;//参数自动填充 这是十多种Excel的一个区分标记 也是服务器上文件名
-	String year;
-	String term;
+	String filetype;//这是十多种Excel的一个区分标记 也是服务器上文件名
+	String year;//学年 在环境中有值
+	String term;//学期
 	String academy;//aid
 	String major;//mid
 	String classs;//cid
-	
+	ServletContext sc;
+	String path;//缓存文件的路径 在Tomcat下
+	{
+		sc = ServletActionContext.getServletContext();
+		path = sc.getRealPath("/download");//得到运行环境路径
+	}
 	//获取文件流
 	public InputStream getFile(){
 		HttpServletRequest request = ServletActionContext.getRequest();
 		ValueStack vs = (ValueStack)request.getAttribute("struts.valueStack");//获取值栈
 		String filename = TurnFileName(filetype);//用户所下载文件的文件名
-		vs.set("filename",filename);
-		
-		
-		return null;
+		vs.set("filename",filename);//压入值栈，在xml文件中获取
+		File file = new File(path, filename);
+		FileInputStream fis = null;
+		try{
+			fis = new FileInputStream(file);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return fis;
 	}
-	/**根据传入type来创建文件，返回文件名 路径写成运行环境下的目录*/
+	/**根据传入type来创建对应文件，返回文件名 路径写成运行环境下的目录*/
 	public String TurnFileName(String type){
 		String filename = "系统有异常.xls";
 		Mysql db = new Mysql("student","root","ad");
-		ResultSet rs = null;
 		List<String[]> list = null;
 		switch (type) {
-		case "g_classs":{
+		case "g_classs":{//某学年某学期某班级的成绩单
 			list = db.SelectReturnList("select classs from classs where cid = '"+classs+"' ");
-			String name = list.get(0)[0];
+			String name = list.get(0)[0];//班级名称
 			filename = name+"班成绩单.xls";
+			String sql = "";
+			Table2Excel.Run(sql, filename, name, path);
 			break;
 		}
 		case "g_major":{
