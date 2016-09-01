622bf0a029cb3bc3565ffe3f6867b2c38f636840
Myth <kuangchengping@outlook.com>
2016-09-01 23:21:02 +0800

    终于实现了动态列的功能，使用的只是简单的多虚表的等值连接，通过程序来拼凑SQL语句

--- src/cn/struts/download/DownloadAction.java
+++ src/cn/struts/download/DownloadAction.java
@@ -56,100 +56,125 @@ public class DownloadAction extends ActionSupport {
 		String filename = "系统有异常.xls";
 		Mysql db = new Mysql("student","root","ad");
 		List<String[]> list = null;
+		
 		switch (type) {
-		case "g_classs":{//某学年某学期某班级的成绩单
-			list = db.SelectReturnList("select classs from classs where cid = '"+classs+"' ");
-			String name = list.get(0)[0];//班级名称
-			filename = name+"班成绩单.xls";
-			
-			String getCol="select o.cno,cname from obligatory o,course c where o.cno=c.cno and cid = '"+classs
-					+"' and year='"+year+"' and term = "+term+" ";
-			list = db.SelectReturnList(getCol);//查出某学期某班级有多少课程
-			String sql[] = new String[list.size()+5];
-			String ColName[] = new String[list.size()+5];
-			//列的名字初始化赋值
-			ColName[0] = "序号";ColName[1] = "学号";ColName[2] = "姓名";
-			ColName[3] = "总学分";ColName[list.size()+4] = "成绩总分";
-			for (int i=0;i<list.size();i++){
-				ColName[i+4] = list.get(i)[1];//填入名字
+			case "g_classs":{//某学年某学期某班级的成绩单
+				list = db.SelectReturnList("select classs from classs where cid = '"+classs+"' ");
+				String name = list.get(0)[0];//班级名称
+				filename = name+"班成绩单.xls";
+				// 0 cno 1 cname
+				String getCol="select o.cno,cname from obligatory o,course c where o.cno=c.cno and cid = '"+classs
+						+"' and year='"+year+"' and term = "+term+" ";
+				list = db.SelectReturnList(getCol);//查出某学期某班级有多少课程
+				//
+				int Nums = list.size();
+				String sql[] = new String[Nums+5];
+				String ColName[] = new String[Nums+5];
+				//列的名字初始化赋值
+				ColName[0] = "序号";ColName[1] = "学号";ColName[2] = "姓名";
+				ColName[3] = "总学分";ColName[Nums+4] = "成绩总分";
+				for (int i=0;i<list.size();i++){
+					ColName[i+4] = list.get(i)[1];//填入名字
+				}
+				//数据部分SQL初始化赋值
+	//			sql[0] = "";
+	//			sql[1] = "select sno from mark where cid = '"+classs +"' and year='"+year+"' and term = "+term+" order by sno";
+	//			sql[2] = "select sname,sno from mark_pro where cid = '"+classs +"' and year='"+year+"' and term = "+term+" order by sno";
+	//			sql[3] = "";
+	//			sql[list.size()+4] = "";
+	//			for (int i=0;i<list.size();i++){
+	//				sql[i+4] = "select grade,sno from mark where cno = '"+list.get(i)[0]+"' and cid = '"+classs +"' and year='"
+	//			+year+"' and term = "+term+" order by sno";
+	//			}
+	//			Table2Excel.CreateDynamicTable(ColName, sql, filename, path);
+				
+				//换一种方法，先将动态的成绩表处理创建成一个临时表，再做处理
+				String drop = "drop table if exists temp ;";
+				db.updSQL(drop);
+				String create = "create table temp as ( select c_"+list.get(0)[0]+".sno,sname";
+				for(int i=0;i<Nums;i++){
+					create +="col_"+i+".grade "+list.get(i)[0]+",col_"+i+".credit cr_"+list.get(i)[0]+",";
+				}
+				create = create.substring(0, create.length()-1);
+				create += " from ";
+				String key = "and year='"+year+"' and term = "+term;
+				for(int i=0;i<Nums;i++){
+					create += "(select sno,grade,credit from mark where course.cno=mark.cno and mark.cno = '"
+							+list.get(i)[0]+"' "+key+" ) col_"+i+" ,";
+				}
+				create +="student s where ";
+//				create = create.substring(0, create.length()-1);
+				for(int i=0;i<Nums-1;i++){
+					create +=" col_"+i+".sno=col_"+(i+1)+".sno and";
+				}
+				create +=" cid = '"+classs +"' );";
+				db.updSQL(create);
+				break;
 			}
-			//数据部分SQL初始化赋值
-			sql[0] = "";
-			sql[1] = "select sno from mark where cid = '"+classs +"' and year='"+year+"' and term = "+term+" order by sno";
-			sql[2] = "select sname,sno from mark_pro where cid = '"+classs +"' and year='"+year+"' and term = "+term+" order by sno";
-			sql[3] = "";
-			sql[list.size()+4] = "";
-			for (int i=0;i<list.size();i++){
-				sql[i+4] = "select grade,sno from mark where cno = '"+list.get(i)[0]+"' and cid = '"+classs +"' and year='"
-			+year+"' and term = "+term+" order by sno";
-			}
-			Table2Excel.CreateDynamicTable(ColName, sql, filename, path);
-			break;
-		}
-		case "g_major":{
-			list = db.SelectReturnList("select major from major where mid = '"+major+"' ");
-			String name = list.get(0)[0];
-			filename = name+"-各班级成绩单.xls";
-			break;
-		}
-		/*case "p_all":{
-			filename = "所有人员信息表.xls";
-			String sql[] = new String [4];
-			String title[] = new String [4];
-			title[0] = "学生信息";
-			title[1] = "教师信息";
-			title[2] = "辅导员信息";
-			title[3] = "管理员信息";
-			sql[0] = "";
-			sql[1] = "";
-			sql[2] = "";
-			sql[3] = "";
-			Table2Excel.Runs(sql, title, path);
-			break;
-		}*/
-		case "o_classs":{
-			list = db.SelectReturnList("select classs from classs where cid = '"+classs+"' ");
-			String name = list.get(0)[0];
-			filename = name+"班级课程表.xls";
-			break;
-		}
-		case "o_major":{
-			list = db.SelectReturnList("select major from major where mid = '"+major+"' ");
-			String name = list.get(0)[0];
-			filename = name+"-各班级课程表.xls";
-			break;
-		}
-		//下面的做成树结构查看或查询
-		/*case "s_major":{
-			if(academy!=null){
-				list = db.SelectReturnList("select academy from major where aid = '"+academy+"' ");
-				String name = list.get(0)[0];
-				filename = name+"-专业信息表.xls";
-			}else{
-				filename = "所有专业信息表.xls";
-			}
-			
-			break;
-		}*/
-		/*case "s_classs":{
-			if(major!=null){
+			case "g_major":{
 				list = db.SelectReturnList("select major from major where mid = '"+major+"' ");
 				String name = list.get(0)[0];
-				filename = name+"-班级信息表.xls";
-			}else{
-				filename = "所有班级信息表.xls";
+				filename = name+"-各班级成绩单.xls";
+				break;
 			}
-			
-			break;
-		}*/
-		/*case "s_academy":{
-			list = db.SelectReturnList("select academy from major where aid = '"+academy+"' ");
-			String name = list.get(0)[0];
-			filename = name+"-学院信息表.xls";
-			break;
-		}*/
-		default:
-			break;
+			/*case "p_all":{
+				filename = "所有人员信息表.xls";
+				String sql[] = new String [4];
+				String title[] = new String [4];
+				title[0] = "学生信息";
+				title[1] = "教师信息";
+				title[2] = "辅导员信息";
+				title[3] = "管理员信息";
+				sql[0] = "";
+				sql[1] = "";
+				sql[2] = "";
+				sql[3] = "";
+				Table2Excel.Runs(sql, title, path);
+				break;
+			}*/
+			case "o_classs":{
+				list = db.SelectReturnList("select classs from classs where cid = '"+classs+"' ");
+				String name = list.get(0)[0];
+				filename = name+"班级课程表.xls";
+				break;
+			}
+			case "o_major":{
+				list = db.SelectReturnList("select major from major where mid = '"+major+"' ");
+				String name = list.get(0)[0];
+				filename = name+"-各班级课程表.xls";
+				break;
+			}
+			//下面的做成树结构查看或查询
+			/*case "s_major":{
+				if(academy!=null){
+					list = db.SelectReturnList("select academy from major where aid = '"+academy+"' ");
+					String name = list.get(0)[0];
+					filename = name+"-专业信息表.xls";
+				}else{
+					filename = "所有专业信息表.xls";
+				}
+				
+				break;
+			}*/
+			/*case "s_classs":{
+				if(major!=null){
+					list = db.SelectReturnList("select major from major where mid = '"+major+"' ");
+					String name = list.get(0)[0];
+					filename = name+"-班级信息表.xls";
+				}else{
+					filename = "所有班级信息表.xls";
+				}
+				
+				break;
+			}*/
+			/*case "s_academy":{
+				list = db.SelectReturnList("select academy from major where aid = '"+academy+"' ");
+				String name = list.get(0)[0];
+				filename = name+"-学院信息表.xls";
+				break;
+			}*/
+			default:
+				break;
 		}
 		return filename;
 	}
